f6b535234be476932bbd492abc131f12b9801f96
Chay Sovannmonich <sovannmonich.chay@student.cadt.edu.kh>
2025-10-26 01:02:47 +0700

    finish models

---
+++ lib/domain/models/doctor_meeting.dart
@@ -0,0 +1,43 @@
+class DoctorMeeting {
+  final String id;
+  final String doctorId;
+  final String title;
+  final String description;
+  final DateTime startTime;
+  final DateTime endTime;
+  final List<String> participantIds;
+  final String  meetingType;
+
+  DoctorMeeting({
+    required this.id,
+    required this.doctorId,
+    required this.title,
+    required this.description,
+    required this.startTime,
+    required this.endTime,
+    required this.participantIds,
+    required this.meetingType,
+  });
+
+  DoctorMeeting copyWith({
+    String? id,
+    String? doctorId,
+    String? title,
+    String? description,
+    DateTime? startTime,
+    DateTime? endTime,
+    List<String>? participantIds,
+    String? meetingType,
+  }) {
+    return DoctorMeeting(
+      id: id ?? this.id,
+      doctorId: doctorId ?? this.doctorId,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      startTime: startTime ?? this.startTime,
+      endTime: endTime ?? this.endTime,
+      participantIds: participantIds ?? this.participantIds,
+      meetingType: meetingType ?? this.meetingType,
+    );
+  }
+}
